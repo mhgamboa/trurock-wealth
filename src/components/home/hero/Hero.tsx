f21620017fd7b967127f5b7c6db9720f693f8f8e
NomadikLiving <marcus@nomadikliving.com>
2023-11-15 19:12:35 -0700

    Update Hero

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import Typewriter from "./Typewriter";
 import financePicture from "@/images/financial-planning.jpg";
 
-export default function Hero() {
+export default function Header() {
   return (
-    <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 -z-10 bg-slate-100">
+    <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 -z-10">
       <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
         <svg
-          className="absolute left-0 hidden h-full text-slate-100 transform -translate-x-1/2 lg:block"
+          className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
           viewBox="0 0 100 100"
           fill="currentColor"
           preserveAspectRatio="none slice"
@@ -15,14 +15,13 @@ export default function Hero() {
           <path d="M50 0H100L50 100H0L50 0Z" />
         </svg>
         <Image
-          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full hidden lg:block"
+          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
           src={financePicture}
-          alt="finance picture"
-          placeholder="blur"
+          alt=""
         />
       </div>
-      <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl py-24">
-        <div className="lg:max-w-lg lg:pr-5">
+      <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
+        <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
             Brand new
           </p>
